26d725179f1157773840805949b52880165c8521
shivangiS04 <shivangimasterblaster@gmail.com>
2025-11-10 00:59:33 +0530

    git commit

--- backend/app.py
+++ backend/app.py
@@ -94,4 +94,5 @@ async def recommend_assessments(request: RecommendRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+
